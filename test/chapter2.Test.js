ba5336a920e8b6379108728e290bc9df171a82b5
frauss <strauss.fred@gmail.com>
2019-06-29 15:47:16 -0400

    Completed Chapter 2 with tests.

--- test/chapter2.Test.js
+++ test/chapter2.Test.js
@@ -14,7 +14,7 @@ function tick(environment, projectile) {
 
 let myProjectile = {
     position: new Point(0, 1, 0),
-    velocity: new Vector(1, 1.8, 0).normalize().multiply(11.25)
+    velocity: new Vector(.5, 1, 0).normalize().multiply(11.75)
 };
 
 let myEnvironment = {
@@ -22,27 +22,32 @@ let myEnvironment = {
     wind: new Vector(-0.01, 0, 0)
 };
 
-let canvasWidth = 900;
-let canvasHeight = 550;
+let canvasWidth = 1000;
+let canvasHeight = 500;
 let myCanvas = new Canvas(canvasWidth, canvasHeight);
 let red = new Color(1, 0, 0);
 
 let tickCount = 1;
 let scaledX = Math.floor(myProjectile.position.x);
-let scaledY = Math.floor(myProjectile.position.y);
+let scaledY = canvasHeight - Math.floor(myProjectile.position.y);
 console.log(`Initial position (${myProjectile.position.x}, ${myProjectile.position.y}, ${myProjectile.position.z}), canvas pos = (${scaledX}, ${scaledY})`);
 myCanvas.writePixel(scaledX, canvasHeight - scaledY, red);
 while (myProjectile.position.y > 0) {
     myProjectile = tick(myEnvironment, myProjectile);
-    let scaledX = Math.floor(myProjectile.position.x);
-    let scaledY = Math.floor(myProjectile.position.y);
+    scaledX = Math.floor(myProjectile.position.x);
+    scaledY = canvasHeight - Math.floor(myProjectile.position.y);
     console.log(`Position after tick ${tickCount} is (${myProjectile.position.x}, ${myProjectile.position.y}), canvas pos = (${scaledX}, ${scaledY})`);
-    if (scaledX < 0 && scaledX > (canvasWidth - 1))
+    if (scaledX < 0 || scaledX > (canvasWidth - 1))
         console.log(`X Pixel off canvas at (${scaledX}, ${scaledY})`);
-    else if (scaledY < 0 && scaledY > (canvasHeight - 1))
+    else if (scaledY < 0 || scaledY > (canvasHeight - 1))
         console.log(`Y Pixel off canvas at (${scaledX}, ${scaledY})`);
     else
-        myCanvas.writePixel(scaledX, canvasHeight - scaledY, red);
+        myCanvas.writePixel(scaledX, scaledY, red);
     tickCount += 1;
 }
-myCanvas.convertToPPM('chapter2.ppm');
+myCanvas.convertToPPM('chapter2.ppm', err => {
+    if (err) {
+        console.error(`Error writing file chapter2.ppm: ${err}`);
+    }
+    process.exit(0);
+});
